3cb2e3a537d9a422b3b1fb29ff889f795a69199b
thoriq-ha <ithoriq0@gmail.com>
2024-07-19 11:36:17 +0700

    feat(test):add greeting test

---
+++ src/Greeting.test.js
@@ -0,0 +1,14 @@
+// src/Greeting.test.js
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect"; // untuk tambahan matchers
+import Greeting from "./Greeting";
+
+test("renders a greeting message", () => {
+  // Render komponen
+  render(<Greeting name="Thoriq" />);
+
+  // Temukan elemen dengan query dan periksa isinya
+  const greetingElement = screen.getByText(/Hello, Thaoriq!/i);
+  expect(greetingElement).toBeInTheDocument();
+});
