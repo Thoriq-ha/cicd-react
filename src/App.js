1b992bd2e3ac5b3e15620e549bc82ec25fbcbd5a
thoriq-ha <ithoriq0@gmail.com>
2024-07-19 17:15:51 +0700

    feat(App): fetching data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/posts"
+        );
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Welcome to My Simple UI React App bbb</h1>
-        <p>This is a for cicd test.</p>
+        <p>This is for CICD test.</p>
       </header>
+      <main>
+        {data ? (
+          <ul>
+            {data.map((item) => (
+              <li key={item.id}>{item.title}</li>
+            ))}
+          </ul>
+        ) : (
+          <p>Loading data...</p>
+        )}
+      </main>
     </div>
   );
 }
