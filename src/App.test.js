083d0949377821a39e30e50fcc3c7743b7a7d5fc
thoriq-ha <ithoriq0@gmail.com>
2024-07-19 17:18:27 +0700

    fix(test): fetching data

--- src/App.test.js
+++ src/App.test.js
@@ -11,6 +11,6 @@ test("renders welcome message", () => {
 
 test("renders CICD test message", () => {
   render(<App />);
-  const cicdTestElement = screen.getByText(/This is a for cicd test/i);
+  const cicdTestElement = screen.getByText(/This is for CICD test./i);
   expect(cicdTestElement).toBeInTheDocument();
 });
