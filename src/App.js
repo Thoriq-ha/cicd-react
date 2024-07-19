50377a22e5297d1caf32b1cb7a72abc8fd9e9da7
thoriq-ha <ithoriq0@gmail.com>
2024-07-19 11:39:34 +0700

    feat(test):add greeting test

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Welcome to My Simple UI React App</h1>
+        <h1>Welcome to My Simple UI React App bbb</h1>
         <p>This is a for cicd test.</p>
       </header>
     </div>
